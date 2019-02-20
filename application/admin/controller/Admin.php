159c1063048c762cb4f315727d9f871e2b869d1b
HenryWong <675666296@qq.com>
2019-02-21 00:52:44 +0800

    初步搭建h-ui.admin后台模板

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+
+class Admin extends Controller
+{
+    public function index()
+    {
+//        return $this->fetch();
+        return '123s';
+    }
+}
---
+++ application/admin/controller/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function Welcome(){
+        return "welcome to thinkPHP5";
+    }
+}
